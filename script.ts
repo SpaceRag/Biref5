8e57fa14b545729900a087ac544ef5d222abbcee
SpaceRag <arno.pimenta@gmaile.com>
2023-03-29 21:31:57 +0200

    Home_css_inprogress

---
+++ script.ts
@@ -0,0 +1,8 @@
+
+//Déclaration de const 
+
+const home: HTMLElement | null = document.querySelector(".tamagotchi")
+const startBtn: HTMLElement | null = document.querySelector(".btn")
+const gekPerso: HTMLElement | null = document.querySelector("#geko")
+const catPerso: HTMLElement | null = document.querySelector("#cat")
+const birdPerso: HTMLElement | null = document.querySelector("#bird")
